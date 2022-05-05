3928fc7c7b8b84a1466916c4bf0c59862f4b536c
ashit <ashitkoladiya7@gmail.com>
2022-05-05 13:50:43 +0530

    5_5second

--- src/ashit/container.js
+++ src/ashit/container.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class container extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            Country : 'india'
-        };
-    }
+function container(props) {
 
-    Country = ()=>{
-        this.setState({
-            Country : 'uk'
-        })
+    const [Country , setcountry]=useState('india');
+    const [gdp,setGDP]=useState(0);
+
+    const Change = ()=>{
+        setcountry('UK');
     }
-    
-    render() {
-        return (
-           <>
-            <div>country:{this.state.Country}</div>
-            <button onClick={()=>this.Country()}>change</button>
-           </>
-        );
+    const ChangeGDP =()=>{
+        setGDP('5.5');
     }
+    return (
+        <div>
+            <p>Country[function based componets]:{Country}</p>
+            <button onClick={()=>Change()}>Change</button>
+
+            <p>GDP[function based componets]:{gdp}</p>
+            <button onClick={()=>ChangeGDP}>Change</button>
+        </div>
+    );
 }
 
 export default container;
