1afacb8f620f060c43e0ac6d925885ae34df9d5a
ashit <ashitkoladiya7@gmail.com>
2022-05-05 13:38:52 +0530

    5_5

--- src/ashit/container.js
+++ src/ashit/container.js
@@ -13,22 +13,15 @@ class container extends Component {
             Country : 'uk'
         })
     }
-
-   
-    
     
     render() {
         return (
            <>
             <div>country:{this.state.Country}</div>
             <button onClick={()=>this.Country()}>change</button>
-
-
-            <div>city :{this.state.City}</div>
-            <button onClick={()=>this.City()}>change</button>
            </>
         );
     }
 }
 
-export default City;
+export default container;
